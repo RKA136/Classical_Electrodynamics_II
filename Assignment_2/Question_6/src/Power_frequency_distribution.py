9828a9687b0c1c405cc9d7c197fbb5620e25d580
Ritam Karmakar <ritamkarmakar19032004@gmail.com>
2025-11-09 13:01:51 +0530

    Question_6 for Assignment_2

--- Assignment_2/Question_6/src/Power_frequency_distribution.py
+++ Assignment_2/Question_6/src/Power_frequency_distribution.py
@@ -5,11 +5,12 @@ from scipy.constants import h, k
 # Thermal Bremsstrahlung Power Spectrum (Up to a Constant Factor)
 
 def bremsstrahlung_power(nu, T, prefactor=1.0):
-    return prefactor * np.exp(-(h * nu) / (k * T)) / np.sqrt(T)
+    x = (h*nu)/(k*T)
+    return prefactor * T**(-0.5)*(2.2*(1+x)+0.6*(x**2))*(np.exp(-x))
 
 
 # Frequency Range
-nu = np.logspace(11, 20, 1200)          
+nu = np.logspace(0, 20, 1200)          
 
 # Temperatures
 T1 = 300
@@ -25,7 +26,8 @@ plt.loglog(nu, P2, linewidth=2.0, label="T = 10⁶ K")
 plt.title("Thermal Bremsstrahlung Power Spectrum\n(Arbitrary Units)", fontsize=14, fontweight="bold")
 plt.xlabel("Frequency ν  [Hz]", fontsize=12)
 plt.ylabel("Power per Unit Frequency  [a.u.]", fontsize=12)
-plt.ylim(10**(-30),1)
+plt.xlim(1,1e20)
+plt.ylim(1e-18,1e3)
 plt.grid(which="both", linestyle="--", linewidth=0.6, alpha=0.7)
 plt.legend(fontsize=11, loc="upper right")
 plt.tight_layout()
